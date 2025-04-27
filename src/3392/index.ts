49f10a3f7b6f49ba276438ba23a3bb9a85a0f6cd
Alex <alexzoetermeer@gmail.com>
2025-04-27 20:49:31 +0200

    added eslint and prettier to force some style in codebase

--- src/3392/index.ts
+++ src/3392/index.ts
@@ -1,54 +1,56 @@
-import { Exercise } from "../exec";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Exercise } from '../exec';
 
 // First attempt, overthinking it lol
 
 function countSubarraysS1(nums: number[]): number {
-  const subarrays = subarray3n(nums);
+    const subarrays = subarray3n(nums);
 
-  return subarrays.reduce((accu, subarray) => {
-    const [first, mid, last] = subarray;
-    return accu + (((first + last) === mid / 2) ? 1 : 0);
-  }, 0);
-};
+    return subarrays.reduce((accu, subarray) => {
+        const [first, mid, last] = subarray;
+        return accu + (first + last === mid / 2 ? 1 : 0);
+    }, 0);
+}
 
 const subarray3n = (nums: number[]): number[][] => {
-  const subarrays: number[][] = [];
-  for (let i = 0; i < nums.length; i++) {
-    subarrays.push([nums[i], nums[i+1], nums[i+2]]);
-  }
+    const subarrays: number[][] = [];
+    for (let i = 0; i < nums.length; i++) {
+        subarrays.push([nums[i], nums[i + 1], nums[i + 2]]);
+    }
 
-  return subarrays.filter(subarray => subarray.every(e => e !== undefined));
-}
+    return subarrays.filter((subarray) => subarray.every((e) => e !== undefined));
+};
 
 // Second solution; on top of the bell curve haha!
 
 function countSubarraysS2(nums: number[]): number {
-  let ans = 0;
+    let ans = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    const [first, mid, last] = [nums[i], nums[i + 1], nums[i + 2]];
-    ans += ((first + last) === mid / 2) ? 1 : 0;
-  }
-  
-  return ans;
-};
+    for (let i = 0; i < nums.length; i++) {
+        const [first, mid, last] = [nums[i], nums[i + 1], nums[i + 2]];
+        ans += first + last === mid / 2 ? 1 : 0;
+    }
+
+    return ans;
+}
 
 // Third solution; beating the bell curve! 87th percentile
 function countSubarrays(nums: number[]): number {
-  let ans = 0;
+    let ans = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    ((nums[i] + nums[i + 2]) === nums[i + 1] / 2) ? ++ans : null;
-  }
-  
-  return ans;
-};
+    for (let i = 0; i < nums.length; i++) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        nums[i] + nums[i + 2] === nums[i + 1] / 2 ? ++ans : null;
+    }
+
+    return ans;
+}
 
 export default {
-  run: () => [
-      { result: countSubarrays([1,2,1,4,1]), expected: 1 },
-      { result: countSubarrays([1,1,1]), expected: 0 },
-      { result: countSubarrays([-1,-2,0]), expected: 1 },
-      { result: countSubarrays([0,0,0,0]), expected: 2 },
-    ]
-} as Exercise
+    run: () => [
+        { result: countSubarrays([1, 2, 1, 4, 1]), expected: 1 },
+        { result: countSubarrays([1, 1, 1]), expected: 0 },
+        { result: countSubarrays([-1, -2, 0]), expected: 1 },
+        { result: countSubarrays([0, 0, 0, 0]), expected: 2 },
+    ],
+} as Exercise;
--- src/exec.ts
+++ src/exec.ts
@@ -2,65 +2,69 @@ import chalk from 'chalk';
 import { ok } from 'assert';
 
 class BootstrapError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'BootstrapError';
-  }
+    constructor(message: string) {
+        super(message);
+        this.name = 'BootstrapError';
+    }
 }
 
 const isExercise = (exercise: unknown): exercise is Exercise => {
-  if (typeof exercise !== 'object' || exercise === null) {
-    return false;
-  }
-  if (typeof (exercise as Exercise).run !== 'function') {
-    return false;
-  }
-  return true;
+    if (typeof exercise !== 'object' || exercise === null) {
+        return false;
+    }
+    if (typeof (exercise as Exercise).run !== 'function') {
+        return false;
+    }
+    return true;
 };
 
 type TestCase = {
-  onlyThis?: boolean;
-  result: unknown;
-  expected: unknown;
-}
+    onlyThis?: boolean;
+    result: unknown;
+    expected: unknown;
+};
 
 export type Exercise = {
-  run: () => TestCase[];
-}
+    run: () => TestCase[];
+};
 
 (async () => {
-  try {
-    ok(process.argv.length > 2, new BootstrapError(chalk`{red.bold No exercise specified in argv}`));
-    const userInput = process.argv[2];
-  
-    const exercise = (await import(`./${userInput}`)).default as Exercise;
+    try {
+        ok(process.argv.length > 2, new BootstrapError(chalk`{red.bold No exercise specified in argv}`));
+        const userInput = process.argv[2];
 
-    ok(isExercise(exercise), new BootstrapError(chalk`{red.bold Exercise ${userInput} does not export a valid exercise}`));
-  
-    console.log(chalk`{bold.blue Executing ${userInput}...}`);
+        const exercise = (await import(`./${userInput}`)).default as Exercise;
 
-    const testcases = exercise.run();
+        ok(
+            isExercise(exercise),
+            new BootstrapError(chalk`{red.bold Exercise ${userInput} does not export a valid exercise}`),
+        );
 
-    const onlySet = testcases.some((testcase) => testcase.onlyThis);
-    const filteredTestcases = onlySet ? testcases.filter((testcase) => testcase.onlyThis) : testcases;
+        console.log(chalk`{bold.blue Executing ${userInput}...}`);
 
-    filteredTestcases.forEach((testcase, idx) => {
-      const passed = testcase.result === testcase.expected;
-      console.log(chalk`{blue Testcase ${idx}} {bold.${passed ? 'green' : 'red'} ${passed ? 'PASSED' : 'FAILED'}}`);
+        const testcases = exercise.run();
 
-      if (!passed) {
-        console.log(chalk`  {bold Expected:} ${testcase.expected}`);
-        console.log(chalk`  {bold Received:} ${testcase.result}`);
-      }
+        const onlySet = testcases.some((testcase) => testcase.onlyThis);
+        const filteredTestcases = onlySet ? testcases.filter((testcase) => testcase.onlyThis) : testcases;
 
-    })
-  } catch (err) {
-    if (err instanceof BootstrapError) {
-      console.error(err.message);
-      process.exit(1);
+        filteredTestcases.forEach((testcase, idx) => {
+            const passed = testcase.result === testcase.expected;
+            console.log(
+                chalk`{blue Testcase ${idx}} {bold.${passed ? 'green' : 'red'} ${passed ? 'PASSED' : 'FAILED'}}`,
+            );
+
+            if (!passed) {
+                console.log(chalk`  {bold Expected:} ${testcase.expected}`);
+                console.log(chalk`  {bold Received:} ${testcase.result}`);
+            }
+        });
+    } catch (err) {
+        if (err instanceof BootstrapError) {
+            console.error(err.message);
+            process.exit(1);
+        }
+
+        console.error((err as Error).message);
+        process.exit(1);
     }
-
-    console.error((err as Error).message);
-    process.exit(1);
-  }
 })();
